8c7a6e92e0c1520e82dc22c828f094e10ae9947d
Marco Antonio Sydney Horta <marco.sh@outlook.com>
2022-06-16 09:50:50 -0300

    Add project files.

---
+++ NumberHave/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace NumberHave
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dados;
+            string[] numeros;
+
+
+            Console.Write("Enter number separeted by space: ");
+            dados = Console.ReadLine();
+
+            numeros = dados.Split(" ");
+            int tam = numeros.Length;
+            int[] inteiros = new int[tam];
+
+            for (int i = 0; i< tam; i++)
+            {
+                inteiros[i] = int.Parse(numeros[i]);
+            }
+            Solution.CountElements(inteiros);
+            Console.WriteLine("Output = " + inteiros[1]);
+            Console.Read();
+        }
+    }
+}
---
+++ NumberHave/Solution.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NumberHave
+{
+    class Solution
+    {
+        static public void CountElements (int[] arr)
+        {
+            // to be returned in arr[1] as requested by the problem
+
+            int tam = arr.Length;
+            int soma;
+            int total = 0;
+
+            for (int i = 0; i < tam; i++)
+            {
+                for (int j = 0; j < tam; j++)
+                {
+                    soma = arr[i] + arr[j];
+                    if (j < tam - 1)
+                    {
+                        if (soma == arr[j + 1])
+                        {
+                            total++;
+                        }
+                    }
+                }
+            }
+            arr[1] = total;
+        } 
+    }
+}
