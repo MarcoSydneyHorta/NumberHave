0f1e6d93bfe29fb5eb29e07fb0dc25b56f418ec9
MarcoSydneyHorta <86072098+MarcoSydneyHorta@users.noreply.github.com>
2022-10-06 13:03:48 -0300

    With only one input

--- NumberHave/Program.cs
+++ NumberHave/Program.cs
@@ -13,6 +13,12 @@ namespace NumberHave
             numeros = Console.ReadLine().Split(" ");
 
             int tam = numeros.Length;
+            if ( tam == 1)
+            {               
+                tam = 2;
+                Array.Resize(ref numeros, tam); // because the output is in inteiros[1]
+                numeros[1] = "0";
+            }
             int[] inteiros = new int[tam];
 
             for (int i = 0; i< tam; i++)
